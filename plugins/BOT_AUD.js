f15d5264c5e8953a949f8c4f4be7647d5578703e
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-01-04 00:03:04 +0530

    Add files via upload

--- plugins/BOT_AUD.js
+++ plugins/BOT_AUD.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./AMEEN-SER/AMEEN/${pickRandom(["ser", "babe", "hihi", "meme", "meera"])}.mp3`
+let av = `./AMEEN-SER/AMEEN/${pickRandom(["ser", "babe", "hihi", "meme", "meera", "ninja", "thuli"])}.mp3`
 
-m.reply( `👻Hello ${taguser} Need Help?  Type .menu `)
+m.reply( `👻Hello ${taguser} 🤍`)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
