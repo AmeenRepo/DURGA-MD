e100b47e1f3cb05d626e3ebfc9eb8805c1b1edf0
Mrimperfect7 <110906985+Mrimperfect7@users.noreply.github.com>
2024-01-03 18:21:09 +0530

    Update fpp.js

--- plugins/fpp.js
+++ plugins/fpp.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
-let res = (await axios.get(`https://gist.githubusercontent.com/Mrimperfect7/873ba6f5eb9df9ca60ad97631cdaf4a4/raw/bbdbac346bb44f9cac7c61b512d55a5593f80b39/fpps.json`)).data  
-let url = await res.files[Math.floor(res.files.length * Math.random())]
+let res = (await axios.get(`https://raw.githubusercontent.com/Mrimperfect7/api/IMPU/BOT-JSON/fpp.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
 conn.sendFile(m.chat, url, 'error.jpg', `*fpp*`, m)} 
-//conn.sendButton(m.chat, "*fpp*", author, url, [[' NEXT ', `${usedPrefix + command}`]], m)}
+//conn.sendButton(m.chat, "*fpp*", author, url, [['NEXT ', `${usedPrefix + command}`]], m)}
 handler.help = ['fpp']
-handler.tags = ['internet']
+handler.tags = ['img']
 handler.command = /^(fpp)$/i
 export default handler
