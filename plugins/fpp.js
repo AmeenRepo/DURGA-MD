94914749e8784a8dfdd1170404b3c5b4a05cea27
Mrimperfect7 <110906985+Mrimperfect7@users.noreply.github.com>
2024-01-03 17:32:06 +0530

    Update fpp.js

--- plugins/fpp.js
+++ plugins/fpp.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://gist.githubusercontent.com/Mrimperfect7/873ba6f5eb9df9ca60ad97631cdaf4a4/raw/cdddb5989b8a0c9ca6efd6c51a40dc130bcf0639/fpps.json`)).data  
-let url = await res[Math.floor(res.length * Math.random())]
+let url = await res.files[Math.floor(res.files.length * Math.random())]
 conn.sendFile(m.chat, url, 'error.jpg', `*fpp*`, m)} 
-//conn.sendButton(m.chat, "*fpp*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
+//conn.sendButton(m.chat, "*fpp*", author, url, [[' NEXT ', `${usedPrefix + command}`]], m)}
 handler.help = ['fpp']
 handler.tags = ['internet']
 handler.command = /^(fpp)$/i
