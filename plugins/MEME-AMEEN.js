bc34a1766cb6af11301d468097e6b3618d58cbbb
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-01-05 01:00:42 +0530

    Add files via upload

--- plugins/MEME-AMEEN.js
+++ plugins/MEME-AMEEN.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   let data = await (await fetch('https://raw.githubusercontent.com/AmeenRepo/database/blob/main/ameen/hehe.json')).json()
   let meera = data[Math.floor(Math.random() * data.length)]
   
-  let ameen = await(await fetch(meera.ameen-meme)).buffer()
+  let ameen = await(await fetch(meera.ameens)).buffer()
   await conn.sendFile(m.chat, ameen, '', '_◽AMEEN-SER_', m)
 }
 handler.help = ['meme', 'memevid']
