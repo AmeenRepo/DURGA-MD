6be9595b1ba12e88c6e31e934e3ba02bfbd66bfb
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-01-07 16:08:36 +0530

    Update MEME-AMEEN.js

--- plugins/MEME-AMEEN.js
+++ plugins/MEME-AMEEN.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/AmeenRepo/database/blob/main/ameen/hehe.json`)).data  
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
 conn.sendFile(m.chat, ronaldo, 'image.jpg', `*HEHE BY AMEEN✌🏻*`, m)}
-conn.sendpoll(m.chat, "*HEHE BY AMEEN✌🏻*", author, "AMEEN", [['NEXT', `${usedPrefix + command}`]], m)}
+//conn.sendpoll(m.chat, "*HEHE BY AMEEN✌🏻*", author, "AMEEN", [['NEXT', `${usedPrefix + command}`]], m)}
 handler.help = ['meme', 'hehe']
 handler.tags = ['internet']
 handler.command = /^(meme|hehe)$/i
