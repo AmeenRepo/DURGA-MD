3fe6e0122e5b413a8d104fa47eaf5401314340b3
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-01-08 00:37:06 +0530

    Add files via upload

--- plugins/mser.js
+++ plugins/mser.js
@@ -1,12 +1,9 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {        
 let res = (await axios.get(`https://raw.githubusercontent.com/AmeenRepo/database/main/ameen/mser.json`)).data  
-let ameen = await axios.get(res[Math.floor(Math.random() * res.length)])
-  let { url } = ameen.data
-
-  conn.sendMessage(m.chat, url, null, { asSticker: true })
-  m.react('🗡️')
-}
+let url = await res[Math.floor(res.length * Math.random())]
+let stiker = await sticker(false, res.url, global.packname, global.author)
+conn.sendFile(m.chat, url, "sticker.webp", { asSticker: true }, m)
 handler.help = ['mser']
 handler.tags = ['internet']
 handler.command = /^(mser)$/i
