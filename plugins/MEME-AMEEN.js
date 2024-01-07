7ce11a185df6f04e44d6a87f29158e7191fae63f
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-01-07 15:57:27 +0530

    AMEEN SER

--- plugins/MEME-AMEEN.js
+++ plugins/MEME-AMEEN.js
@@ -1,15 +1,10 @@
-import fetch from "node-fetch"
-let handler = async (m, { conn }) => {
-
-  let data = await (await fetch('https://raw.githubusercontent.com/AmeenRepo/database/blob/main/ameen/hehe.json')).json()
-  let meera = data[Math.floor(Math.random() * data.length)]
-  
-  let ameen = await(await fetch(meera.ameens)).buffer()
-  await conn.sendFile(m.chat, ameen, '', '_◽AMEEN-SER_', m)
-}
-handler.help = ['meme', 'memevid']
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let cristiano = (await axios.get(`https://raw.githubusercontent.com/AmeenRepo/database/blob/main/ameen/hehe.json`)).data  
+let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
+conn.sendFile(m.chat, ronaldo, 'image.jpg', `*HEHE BY AMEEN✌🏻*`, m)}
+conn.sendpoll(m.chat, "*HEHE BY AMEEN✌🏻*", author, "AMEEN", [['NEXT', `${usedPrefix + command}`]], m)}
+handler.help = ['meme', 'hehe']
 handler.tags = ['internet']
-handler.command = ['meme','memevid'] 
-
-
+handler.command = /^(meme|hehe)$/i
 export default handler
