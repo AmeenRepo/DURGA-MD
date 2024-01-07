1b7b332cef777dbd1bd7c59d5c36680856bd6e21
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-01-07 16:27:48 +0530

    Add files via upload

--- plugins/MEME-AMEEN.js
+++ plugins/MEME-AMEEN.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
-let cristiano = (await axios.get(`https://raw.githubusercontent.com/AmeenRepo/database/blob/main/ameen/hehe.json`)).data  
-let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
-conn.sendFile(m.chat, ronaldo, 'image.jpg', `*HEHE BY AMEEN✌🏻*`, m)}
-//conn.sendpoll(m.chat, "*HEHE BY AMEEN✌🏻*", author, "AMEEN", [['NEXT', `${usedPrefix + command}`]], m)}
-handler.help = ['meme', 'hehe']
+let res = (await axios.get(`https://raw.githubusercontent.com/AmeenRepo/database/blob/main/ameen/hehe.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `*HEHE BY AMEEN 🕊️*`, m)} 
+//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
+handler.help = ['hehe']
 handler.tags = ['internet']
-handler.command = /^(meme|hehe)$/i
+handler.command = /^(hehe)$/i
 export default handler
