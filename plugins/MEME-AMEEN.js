8f67138c3cc56734bb51d79cf87ef50c58a3626c
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-01-07 16:44:10 +0530

    Add files via upload

--- plugins/MEME-AMEEN.js
+++ plugins/MEME-AMEEN.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/AmeenRepo/database/blob/main/ameen/hehe.json`)).data  
+let res = (await axios.get(`https://raw.githubusercontent.com/AmeenRepo/database/main/ameen/hehe.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
 conn.sendFile(m.chat, url, 'error.jpg', `*HEHE BY AMEEN 🕊️*`, m)} 
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
